00df5901c38d741fe9ee3b0826c9a57c987f47fb
Orlov Oleksandr <oleksandrorlovit@gmail.com>
2025-07-16 12:23:43 +0200

    First commit

---
+++ src/test/java/ua/orlov/betreactive/BetReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.orlov.betreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
